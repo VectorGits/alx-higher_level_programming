f43e347b33f9ba78060644db748ff58a80ea69be
VectorGits <davidoluremi0@gmail.com>
2023-11-29 19:37:51 +0100

    Fil'd files with code :)

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def islower(x):
+	if ord(x) >= 97 and ord(x) <= 122:
+		return True
+	else:
+		return False
+# Alternative solution for code cats :)
+# def islower(c):
+#     return 97 <= ord(c) <= 122
