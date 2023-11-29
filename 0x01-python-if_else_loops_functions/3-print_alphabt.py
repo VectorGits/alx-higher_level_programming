7e122a2c7e21dffddebde0a9fa171bf0b5183336
VectorGits <davidoluremi0@gmail.com>
2023-11-29 09:32:01 -0800

    modded T-3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print("".join([chr(x) for x in range(97, 123) if chr(x) not in ['q', 'e']]), end='')
+print("".join(
+    ["{}".format(chr(x)) for x in range(97, 123) if chr(x) not in ['q', 'e']]
+), end='')
