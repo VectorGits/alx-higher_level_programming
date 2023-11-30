9bf5cc536019c6769aab017b2b7f527f49b13c8b
VectorGits <davidoluremi0@gmail.com>
2023-11-30 22:24:53 +0100

    Added file solution code T-4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/bash/python3
+if __name__ == "__main__":
+    # Print names defined by hidden_4 module ./hidden_4.pyc
+    import hidden_4
+    for x in dir(hidden_4):
+        if x[:2] != "__":
+            print(x)
