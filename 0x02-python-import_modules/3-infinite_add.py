8f8ec05afa30fc1b5daec12b229e628fe1e456ea
VectorGits <davidoluremi0@gmail.com>
2023-11-30 22:14:50 +0100

    Added file solution code

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv)
+    argv_int = [int(sys.argv[x]) for x in range(1, argc)]
+    print(sum(argv_int))
+    
+    # Alternative solution for copycats :P
+    # sum = 0
+    # for x in range(1, argc):
+    # 	sum += int(sys.argv[x])
+    # print(sum)
