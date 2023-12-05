33878b4c6bcd5d218f31e8799bfb0ae2c215d75c
VectorGits <davidoluremi0@gmail.com>
2023-12-05 15:39:37 +0100

    Modded T-3 code

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for x in reversed(my_list):
-        print("{:d}".format(x))
+    if my_list:
+        for x in reversed(my_list):
+         print("{:d}".format(x))
+
+my_list = [1, 2, 3, 4, 5]
+print_reversed_list_integer(my_list)
