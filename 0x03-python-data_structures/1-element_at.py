e8c0a2c346ebe6e740056ab5e2ab31d40fb4fdc2
VectorGits <davidoluremi0@gmail.com>
2023-12-05 14:10:10 +0100

    Modded T-1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list) - 1:
-        return None
-    else:
-        return my_list[idx]
+    if idx < 0:
+        return (None)
+
+    length = len(my_list)
+
+    if idx > length - 1:
+        return (None)
+
+    return(my_list[idx])
     
 my_list = [1, 2, 3, 4, 5]
 idx = 3 
