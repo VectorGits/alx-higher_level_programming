a17027f6821549763f08ea420758aba5ca4d6b81
VectorGits <davidoluremi0@gmail.com>
2023-12-08 07:41:39 +0100

    Modded comment

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    # initialize emty dictionary :)
+    # initialize emty dictionary :^)
     new_dict = {}
     for key, value in a_dictionary.items():
         new_dict[key] = value * 2
