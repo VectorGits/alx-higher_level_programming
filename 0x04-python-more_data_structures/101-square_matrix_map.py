22585dc4b70770295c9b7058668d1d94845ccc88
VectorGits <davidoluremi0@gmail.com>
2023-12-08 22:40:38 +0100

    Refactor square_matrix_map function

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    res_list = list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
-    return res_list
+    return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
