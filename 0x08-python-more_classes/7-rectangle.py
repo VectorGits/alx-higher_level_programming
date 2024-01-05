3fb00215297d79e7a0a51eb23be63c139c8cb81d
VectorGits <davidoluremi0@gmail.com>
2024-01-05 09:48:51 +0100

    Fix formatting in Rectangle class

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -6,10 +6,10 @@ Rectangle
 
 class Rectangle:
     """Represents a rectangle"""
-    
+
     number_of_instances = 0
     print_symbol = '#'
-    
+
 
     def __init__(self, width=0, height=0):
         """Initializes a new rectangle
