9015981fafffe7d06a7ba14c66b49118a04b62b0
VectorGits <davidoluremi0@gmail.com>
2024-01-10 05:59:19 +0100

    Add save_to_json_file function

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module contains a function that writes an Object to a text file, using a JSON representation"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Function that writes an Object to a text file, using a JSON representation
+    
+    Arguments:
+        my_obj (obj): object to be converted to JSON
+        filename (str): filename to write to
+    """
+    with open(filename, mode='w', encoding='utf-8') as file:
+        json.dump(my_obj, file)
