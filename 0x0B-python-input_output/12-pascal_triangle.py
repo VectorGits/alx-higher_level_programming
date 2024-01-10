b201d25505de8a664ad461ab2c4792a810fa4dde
VectorGits <davidoluremi0@gmail.com>
2024-01-10 16:36:53 +0100

    Add pascal_triangle function to generate Pascal's triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module for pascal_triangle"""
+
+
+def pascal_triangle(n):
+    """Returns a list of lists of integers representing the Pascal’s triangle
+    Args:
+        n (int): number of rows in triangle
+    Returns: list of lists of integers
+    """
+    if n <= 0:
+        return[]
+    triangle =[]
+    for x in range(n):
+        row = [1] * (x + 1)
+    for y in range(1, x):
+        row[y] = triangle[x - 1][y - 1] + triangle[x - 1][y]
+    triangle.append(row)
+    return triangle
