1da791c432793bb40a90201b94bb76eea03a5138
VectorGits <davidoluremi0@gmail.com>
2024-01-10 15:27:12 +0100

    Refactor import statements and function calls in 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,19 +3,18 @@
 and then save them to a file
 """
 import sys
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+import os.path
 
 
-# from 5-save_to_json_file import save_to_json_file
-# from 6-load_from_json_file import load_from_json_file
+save_file = __import__('5-save_to_json_file').save_to_json_file
+load_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
 try:
-    my_list = load_from_json_file(filename)
+    my_list = load_file(filename)
 except FileNotFoundError:
     my_list = []
 
 my_list.extend(sys.argv[1:])
-save_to_json_file(my_list, filename)
+save_file(my_list, filename)
