ef625c311f946527fd13438ddb4054e262cc6ea6
VectorGits <davidoluremi0@gmail.com>
2024-01-10 05:59:11 +0100

    Add from_json_string function to convert JSON string to object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!.usr/bin/python3
+"""Module contains a function that returns an object (Python data structure) represented by a JSON string"""
+import json
+
+
+def from_json_string(my_str):
+    """Function that returns an object (Python data structure) represented by a JSON string
+
+    Arguments:
+        my_str (str): JSON string to be converted to object
+        
+    Returns:
+        obj: object represented by JSON string
+    """
+
+    return json.loads(my_str)
