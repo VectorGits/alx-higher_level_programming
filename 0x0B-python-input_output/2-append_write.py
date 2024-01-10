65f9ffcd955e7e3b3be9fe378ba98bb3b6a0b5d4
VectorGits <davidoluremi0@gmail.com>
2024-01-10 05:58:58 +0100

    Add append_write function to append text to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module that contains a function that appends to a text file"""
+
+
+def append_write(filename="", text=""):
+    """Function that appends to a text file
+
+    Keyword Arguments:
+        filename {str} -- filename (default: {""})
+        text {str} -- text to append (default: {""})
+        
+    Raises:
+        Exception: when the file can be opened
+        
+    """
+
+    with open(filename, mode='a', encoding='utf-8') as file:
+        return file.write(text)
