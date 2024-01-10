764ca054459c1e286c442ac48273c4287f10cb46
VectorGits <davidoluremi0@gmail.com>
2024-01-10 15:49:21 +0100

    Add Student class with to_json and reload_from_json methods

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module for Student class"""
+
+
+class Student:
+    """Class for jsonification"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization of Student object
+
+        Args:
+            first_name (str): first name of student
+            last_name (str): last name of student
+            age (int): age of student
+
+        Returns: None
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns dictionary representation of Student instance"""
+        if isinstance(attrs, list) and all(isinstance(attrs, str) for attr in attrs):
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of Student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)
