8f11a974045344b2f1b25992e10593ab200706f6
VectorGits <davidoluremi0@gmail.com>
2024-01-11 17:48:02 +0100

    Add function to add attribute to an object

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Module that defines a function add_attribute """
+
+
+def add_attribute(obj, name, value):
+    """ Function that adds a new attribute to an object if it’s possible
+
+    Args:
+        obj: object to add the attribute to
+        name: name of the attribute
+        value: value of the attribute
+
+    Raises:
+        TypeError: if the attribute cannot be added
+
+    """
+
+    if isinstance(obj, (int, str, float, list, dict, set, tuple, bool, type(None))):
+        raise TypeError("can't add new attribute")
+    setattr(obj, name, value)
