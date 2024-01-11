38519a59c479dfb923f878857e3c8bce169789ab
VectorGits <davidoluremi0@gmail.com>
2024-01-11 17:32:18 +0100

    Add Square class that inherits from Rectangle

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Module that defines a class BaseGeometry """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Class that defines a Square from Rectangle """
+
+    def __init__(self, size):
+        """ Method that initializes the class
+
+        Args:
+            size: size of the square
+
+        """
+
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ Method that returns a string with the area """
+        return super().area()
+
+    def __str__(self):
+        """ Method that returns the string representation of the object """
+        return "[Square] {}/{}".format(self.__size, self.__size)
