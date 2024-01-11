3f22dc4292f794ca323564ceebd8a6963dc893d9
VectorGits <davidoluremi0@gmail.com>
2024-01-11 17:37:26 +0100

    Remove commented out code in Rectangle class

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -20,11 +20,3 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    # def __str__(self):
-    #     """ Method that returns the string representation of the object """
-    #     return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-    # def area(self):
-    #     """ Method that returns the area of the object """
-    #     return self.__width * self.__height
