78beafc643b5329fdd032bc0338d0543eb3df8b4
VectorGits <davidoluremi0@gmail.com>
2024-01-19 19:46:55 +0100

    Add Rectangle class with getters and setters

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" Module that contains class Rectangle """
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ The Rectangle class """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initializes instances """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """ Getter for width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter for width """
+
+    @property
+    def height(self):
+        """ Getter for height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter for height """
+
+    @property
+    def x(self):
+        """ Getter for x """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ Setter for x """
+
+    @property
+    def y(self):
+        """ Getter for y """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ Setter for y """
