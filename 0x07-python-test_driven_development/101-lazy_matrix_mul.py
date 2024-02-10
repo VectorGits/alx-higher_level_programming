a73b6697142930cd363278ce824f70cc07b33a1b
VectorGits <davidoluremi0@gmail.com>
2024-02-10 17:52:15 +0100

    Add lazy_matrix_mul function for matrix multiplication

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module contains the lazy_matrix_mul function
+"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    This function multiplies two matrices
+    """
+
+    return np.dot(m_a, m_b)
