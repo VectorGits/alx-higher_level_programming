12df81d47f5adf239b1eef6df6f0850efe9f2de5
VectorGits <davidoluremi0@gmail.com>
2024-02-10 15:58:26 +0100

    Fix type pycodestyle error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,8 +3,10 @@
 This is the "add_integer" module.
 """
 
+
 def add_integer(a, b=98):
     """Return the sum of two integers."""
+
     if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
     if not (isinstance(b, int) or isinstance(b, float)):
