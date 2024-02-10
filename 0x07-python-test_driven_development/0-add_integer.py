ca7f7e3687ae261f36ab9871837f9cb457e2babe
VectorGits <davidoluremi0@gmail.com>
2024-02-10 15:41:21 +0100

    Add implementation of add_integer function

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is the "add_integer" module.
+"""
+
+def add_integer(a, b=98):
+    """Return the sum of two integers."""
+    if not (isinstance(a, int) or isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if not (isinstance(b, int) or isinstance(b, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
