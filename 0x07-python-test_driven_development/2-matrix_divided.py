edc32c918841f7362bd5a0315782216917cb3aa1
VectorGits <davidoluremi0@gmail.com>
2024-02-10 16:12:44 +0100

    Refactor matrix_divided function to improve readability and maintainability

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,18 +5,18 @@ This is the "matrix_divided" module.
 
 
 def matrix_divided(matrix, div):
-	"""Divide all elements of a matrix by a number."""
-	
-	if not isinstance(matrix, list):
-		raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-	if not all(isinstance(row, list) for row in matrix):
-		raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-	if not all(isinstance(num, (int, float)) for row in matrix for num in row):
-		raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-	if not all(len(row) == len(matrix[0]) for row in matrix):
-		raise TypeError("Each row of the matrix must have the same size")
-	if not (isinstance(div, int) or isinstance(div, float)):
-		raise TypeError("div must be a number")
-	if div == 0:
-		raise ZeroDivisionError("division by zero")
-	return [[round(num / div, 2) for num in row] for row in matrix]
+    """Divide all elements of a matrix by a number."""
+
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(num, (int, float)) for row in matrix for num in row):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+    if not (isinstance(div, int) or isinstance(div, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    return [[round(num / div, 2) for num in row] for row in matrix]
