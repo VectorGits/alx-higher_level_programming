6031de54e14ca55c29e20ecab8b162e0e4519c3d
VectorGits <davidoluremi0@gmail.com>
2024-03-13 23:56:51 +0100

    Refactor user creation SQL query

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,5 @@
 -- Script that creates the MySQL server user user_0d_1.
 -- set passeord to "user_od_1_pwd" and Grant all privileges on all databases of the MySQL server to user_0d_1.
 
-CREATE USER IF NOT EXISTS 'user_od_1'@'localhost'
-IDENTIFIED BY 'user_od_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_od_1'@'localhost' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
