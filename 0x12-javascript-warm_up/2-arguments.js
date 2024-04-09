e2b3678db93d1a7369e4d75e894e7b2954799062
VectorGits <davidoluremi0@gmail.com>
2024-04-09 05:32:39 +0100

    Add JavaScript file with argument checking logic

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+
+// Get args
+const args = process.argv.slice(2);
+
+// Check no of Args
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
