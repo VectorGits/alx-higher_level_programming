f43d18bb42a9276b1ecfe7c40cd92457c36b92c8
VectorGits <davidoluremi0@gmail.com>
2024-04-10 16:00:28 +0100

    Add new JavaScript files and test cases for map function

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+
+const newDict = {};
+
+for (const [userId, occurrences] of Object.entries(dict)) {
+  if (!newDict[occurrences]) {
+    newDict[occurrences] = [userId];
+  } else {
+    newDict[occurrences].push(userId);
+  }
+}
+
+console.log(newDict);
