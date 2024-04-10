6c8a580d4cdc82c0da3cf089e63c06d9221c0aec
VectorGits <davidoluremi0@gmail.com>
2024-04-10 16:20:40 +0100

    Fix file path issue in concatenation functionality

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+const path = require('path');
 
 const fileA = process.argv[2];
 const fileB = process.argv[3];
 const fileC = process.argv[4];
 
-const contentA = fs.readFileSync(fileA, 'utf-8');
-const contentB = fs.readFileSync(fileB, 'utf-8');
+const contentA = fs.readFileSync(path.join(__dirname, fileA), 'utf-8');
+const contentB = fs.readFileSync(path.join(__dirname, fileB), 'utf-8');
 
-const contentC = contentA + '\n' + contentB;
-
-fs.writeFileSync(fileC, contentC);
+fs.writeFileSync(path.join(__dirname, fileC), contentA + contentB);
