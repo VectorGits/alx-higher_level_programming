719a28afab0261da2129dc8a59f887ed8168d5e2
VectorGits <davidoluremi0@gmail.com>
2024-04-11 17:33:56 +0100

    Fix indentation and newline issues

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
-
 // Get all args
 const args = process.argv.slice(2);
 
 // Check if first arg exists and prints the first arg
 if (args[0] !== undefined) {
-	console.log(args[0]);
+  console.log(args[0]);
 } else {
-	console.log('No argument');
+  console.log('No argument');
 }
