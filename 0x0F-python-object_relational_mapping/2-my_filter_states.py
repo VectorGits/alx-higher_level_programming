a8f834964b4c898b0b59e5978ef1949607620069
VectorGits <davidoluremi0@gmail.com>
2024-04-16 03:50:24 +0100

    Refactor code to filter and print states matching a specific name from the database

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
 """
-Takes in an argument and displays all values in the
-states table of hbtn_0e_0_usa where name matches the argument.
+This script lists all states from the database hbtn_0e_0_usa
 """
-
+from sys import argv
 import MySQLdb
-import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    # Connect to MySQL server with provided credentials
+    db = MySQLdb.connect(user=argv[1], port=3306, host="localhost",
+                         passwd=argv[2], db=argv[3])
+
+    # Create cursor object
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name \
-                = %s ORDER BY id ASC", (sys.argv[4],))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+
+    # Execute SQL query to get all states with the provided state name
+    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(argv[4]))
+
+    # Fetch all states
+    states = cur.fetchall()
+
+    # Display results
+    for state in states:
+        if state[1] == argv[4]:
+            print(state)
+
+    # Close cursor and database connection
     cur.close()
     db.close()
