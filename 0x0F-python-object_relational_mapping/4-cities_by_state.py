6a11fe29681539e5b81d0077f8a4005d6917bcc1
VectorGits <davidoluremi0@gmail.com>
2024-04-16 03:53:19 +0100

    Refactor code to use parameterized queries and fix indentation in 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 """
-Lists all cities from the database hbtn_0e_4_usa
+This script lists all cities from the database hbtn_0e_4_usa
 """
 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM cities ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    # Connect to MySQL server with provided credentials
+    connection = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+
+    # Create cursor object
+    cur = connection.cursor()
+
+    # Execute SQL query to get all cities with corresponding state names
+    cur.execute("""SELECT cities.id, cities.name, states.name
+                    FROM cities JOIN states ON cities.state_id = states.id
+                    ORDER BY cities.id ASC""")
+
+    # Fetch all results and print them using a list comprehension
+    [print(city) for city in cur.fetchall()]
