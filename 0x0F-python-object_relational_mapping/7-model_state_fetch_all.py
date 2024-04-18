847cec141ef21462edcb2f78333a769f98fd5063
VectorGits <davidoluremi0@gmail.com>
2024-04-18 19:53:41 +0100

    Refactor code to use SQLAlchemy select statement for fetching and printing all State objects

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
+"""Start link class to table in database
 """
-This script lists all State objects from the database hbtn_0e_6_usa
-"""
-
-from sys import argv
-from sqlalchemy import create_engine
-from model_state import State
-from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, select
 
 if __name__ == "__main__":
-    # Create database engine
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    with engine.connect() as connection:
+        query = select(State).order_by(State.id.asc())
+        states = connection.execute(query)
+        for state in states:
+            print(f"{state.id}: {state.name}")
 
-    # Create session object
-    session_maker = sessionmaker(bind=engine)
-    session = session_maker()
-
-    # Query all State objects and print their ids and names
-    for state in session.query(State).order_by(State.id):
-        print("{}: {}".format(state.id, state.name))
+    engine.dispose()
