1fc7f4f43c553b77c404f1828adc4e85dd95c467
VectorGits <davidoluremi0@gmail.com>
2024-04-18 20:00:44 +0100

    Refactor code to use SQLAlchemy select statement for fetching and printing state objects with names containing 'a'

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
+"""Start link class to table in database
 """
-This script prints the first state object from the database hbtn_0e_6_usa
-"""
-
-from sys import argv
-from sqlalchemy import create_engine
-from model_state import State
-from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, select
 
 if __name__ == "__main__":
-    # Create database  engine
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    with engine.connect() as connection:
+        query = select(State).order_by(State.id.asc()) \
+            .where(State.name.like("%a%"))
+        states = connection.execute(query)
+        for state in states:
+            print(f"{state.id}: {state.name}")
 
-    # Create a new session object  which handle database connections
-    session_maker = sessionmaker(bind=engine)
-
-    # Create a new session object
-    session = session_maker()
-
-    # Query the State table for first state
-    state = session.query(State).order_by(State.id).first()
-
-    # Print the State object's id and name, or nothing if state is not found
-    print("Nothing" if state is None else f"{state.id}: {state.name}")
+    engine.dispose()
