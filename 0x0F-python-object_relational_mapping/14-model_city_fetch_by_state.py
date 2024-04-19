ffe75be22a1e61a88c1115c0c79a2a6379453e64
VectorGits <davidoluremi0@gmail.com>
2024-04-19 13:40:35 +0100

    Refactor code to use SQLAlchemy ilike and update state name to 'New Mexico'

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
-"""Fetch all City objects from the database hbtn_0e_14_usa"""
-import sys
+"""model state
+"""
+from sqlalchemy import create_engine
 from model_state import Base, State
 from model_city import City
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import sys
 
 if __name__ == "__main__":
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
             sys.argv[1], sys.argv[2], sys.argv[3]
         ),
-        pool_pre_ping=True
+        pool_pre_ping=True,
     )
-
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    # Fetch all City objects, sorted by cities.id
-    cities = session.query(City, State).join(
-        State, City.state_id == State.id).order_by(City.id).all()
-
-    # Print each City
-    for city, state in cities:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    result = session.query(City, State).\
+        join(State, State.id == City.state_id).all()
+    if result:
+        for city, state in result:
+            print(f"{state.name}: ({city.id}) {city.name}")
+    session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
+"""model state
+"""
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
 
-class City(Base):
-    """
-    City class
 
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store cities.
-        id (sqlalchemy.Column): The city's id. It's an auto-generated, unique integer, can’t be null and is a primary key.
-        name (sqlalchemy.Column): The city's name. It's a string of 128 characters and can’t be null.
-        state_id (sqlalchemy.Column): The id of the state the city belongs to. It's an integer, can’t be null and is a foreign key to states.id.
-    """
+class City(Base):
+    """ City class """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
