a39219dab2934dfd7c9fee371a04c3c8870270cd
VectorGits <davidoluremi0@gmail.com>
2024-05-23 19:02:16 +0100

    chore: Refactor code to improve file writing

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,8 +6,9 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf-8', (err) => {
-  if (err) {
-    console.log(err);
-  }
-});
+fs.writeFile(filePath, content, 'utf-8',
+  function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
