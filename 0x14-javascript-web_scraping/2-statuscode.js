c50ab6146a811a0105319112e845382cf102e0ea
VectorGits <davidoluremi0@gmail.com>
2024-05-23 19:27:01 +0100

    Refactor to fit standard js style guide

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,9 +5,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (error, response) {
-	if (error) {
-		console.error(error);
-		return;
-	}
-	console.log(`code: ${response.statusCode}`);
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(`code: ${response.statusCode}`);
 });
