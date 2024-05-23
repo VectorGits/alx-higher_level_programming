63590b8f5258a74adca74924bdc88ec1ef537b3b
VectorGits <davidoluremi0@gmail.com>
2024-05-23 18:36:38 +0100

    chore: Refactor code to improve file reading and printing

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-// Read from a file
+// Read from file
 
-const fs = require('fs');
-
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf8', (err, data) => {
-	if (err) {
-		console.log(err);
-		return;
-	}
-	console.log(data);
-});
+const filesys = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+  function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
